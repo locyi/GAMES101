0ea1fdd1aa3e00bee4534cb325879c3a8b212d65
locyi <211069640xzc@gmail.com>
2026-04-03 23:51:24 +0800

    实现任意数量点bezier曲线生成与反走样

--- Assignment4/code/main.cpp
+++ Assignment4/code/main.cpp
@@ -32,13 +32,37 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
 
 cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
 {
+    std::vector<cv::Point2f> mid_points;
+    for (int i = 0; i < control_points.size()-1; i++){
+        float x = control_points[i].x*(1-t) + control_points[i+1].x*t;
+        float y = control_points[i].y*(1-t) + control_points[i+1].y*t;
+        mid_points.push_back(cv::Point2f(x, y));
+    }
+    
+    if (mid_points.size() == 1){
+        return mid_points[0];
+    }
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
+    return recursive_bezier(mid_points, t);
 
 }
 
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
+    for (double t = 0.0; t <= 1.0; t += 0.001){
+        auto point = recursive_bezier(control_points, t);
+        int centralPixel[2] = {(int)std::trunc(point.x),(int)std::trunc(point.y)};
+        float nearbyPixel[4][2] = {{-0.5,0.5},{0.5,1.5},{1.5,0.5},{0.5,-0.5}};  //以中心点的上下左右中心坐标采样
+        for (int i = 0; i < 4; i++){
+            float instance = 0.0;
+            float x = centralPixel[0] + nearbyPixel[i][0];
+            float y = centralPixel[1] + nearbyPixel[i][1];
+            instance = sqrtf(powf(point.x-x, 2)+powf(point.y-y, 2));
+            int nP[2] = {centralPixel[0] + (int)(nearbyPixel[i][0] - 0.5f), centralPixel[1] + (int)(nearbyPixel[i][1] - 0.5f)}; //相邻坐标的绝对坐标
+            if (window.at<cv::Vec3b>(nP[1], nP[0])[1] < 231.818*instance - 115.909) window.at<cv::Vec3b>(nP[1], nP[0])[1] = 231.818*instance - 115.909;
+        }
+        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+    }
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
 
@@ -62,8 +86,8 @@ int main()
 
         if (control_points.size() == 4) 
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            //naive_bezier(control_points, window);
+            bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
